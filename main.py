b6257028181aff90fc886f931de27a44b83f0745
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2022-11-09 12:25:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ number = 1000
 
 text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 
-limit = 10
-
 tag = ['mil', 'milhão', 'bilhão', 'trilhão']
 
 def abbreviator(element, limit=10):
