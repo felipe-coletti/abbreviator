e976ce1b9f3c47fa06a0ff74c3a5029f92d530d9
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2022-11-09 09:45:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-number = 10000
+number = 1000000
 
 text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 
-limit = 500
+limit = 10
 
 tag = ['mil', 'mil', 'milhão', 'milhões', 'bilhão', 'bilhões', 'trilhão', 'trilhões']
 
 
-def abbreviator(element, limit=500):
+def abbreviator(element, limit=10):
   element = str(element)
   if element.isnumeric():
     for i in range(4, len(element), 3):
       if len(element) != 4:
-        if len(element) >= i and len(element) <= (i + 2):
+        if len(element) >= i and len(element) <= (i + 3):
           element = int(element)
           element /= (10 ** (i - 1))
           if element < 2:
@@ -24,18 +24,17 @@ def abbreviator(element, limit=500):
           else:
             element = '{:.1f} {}'.format(element, tag[index])
   else:
-    if limit > len(element):
+    if limit > len(element) - 1:
       limit = len(element) - 1
-      while element[limit] == '':
-        limit -= 1
-    while element[limit] == '':
+    while element[limit] == ' ' or element[limit] == '.':
       limit -= 1
-    while element[limit].isalnum() and element[limit + 1] != '':
+    while element[limit].isalnum() and element[limit + 1] != ' ' and element[limit + 1] != '.':
       limit += 1
-    for i in range(limit + 1, limit + 4):
-      element[i] = '.'
-    for i in range(limit + 4, len(element)):
-      element.remove(element[i])
+    while len(element) - 1 > limit:
+      element = element[:-1]
+    while element[len(element) - 1] == '.':
+      element = element[:-1]
+    element += '...'
   return element
 
 
