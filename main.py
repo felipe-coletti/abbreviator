3052efdcf54d8ce16f74853e2ac7eb48f1aa20c0
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2022-11-09 01:17:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,19 @@ limit = 500
 
 def abbreviator(element, limit):
   if element.isdigit():
-    if len(element) >= 4 and len(element) < 7:
+    if len(element) >= 4 and len(element) <= 6:
       element /= 1000
       element = "{:,2f} mil".format(element)
   else:
+    while element(limit) == '':
+      limit -= 1
+    while element(limit).isalpha and element(limit + 1):
+      limit += 1
     for i in range(limit, limit + 4):
       element[i] = '.'
     for i in range(limit + 4, len(element)):
       element.remove[i]
+  return element
 
+
+print(abbreviator(number))
