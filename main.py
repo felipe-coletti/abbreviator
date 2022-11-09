2df67395532e5ca186f9228055aea994d78af113
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2022-11-09 09:07:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-number = 1000
+number = 10000
 
 text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 
@@ -7,29 +7,35 @@ limit = 500
 tag = ['mil', 'mil', 'milhão', 'milhões', 'bilhão', 'bilhões', 'trilhão', 'trilhões']
 
 
-def abbreviator(element, limit):
-  if element.isdigit():
+def abbreviator(element, limit=500):
+  element = str(element)
+  if element.isnumeric():
     for i in range(4, len(element), 3):
       if len(element) != 4:
         if len(element) >= i and len(element) <= (i + 2):
+          element = int(element)
           element /= (10 ** (i - 1))
           if element < 2:
             index = i - 5
           else:
             index = i - 4
-          if not element.isdecimal():
+          if not str(element).isdecimal():
             element = '{:.0f} {}'.format(element, tag[index])
           else:
             element = '{:.1f} {}'.format(element, tag[index])
   else:
-    while element(limit) == '':
+    if limit > len(element):
+      limit = len(element) - 1
+      while element[limit] == '':
+        limit -= 1
+    while element[limit] == '':
       limit -= 1
-    while element(limit).isalpha and element(limit + 1):
+    while element[limit].isalnum() and element[limit + 1] != '':
       limit += 1
     for i in range(limit + 1, limit + 4):
       element[i] = '.'
     for i in range(limit + 4, len(element)):
-      element.remove[i]
+      element.remove(element[i])
   return element
 
 
