c090bebf5c1f45c4cfa9ac9bd27700ef4d9ea5f5
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2022-11-09 10:39:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-number = 1000000
+number = 1000
 
 text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 
@@ -10,15 +10,17 @@ tag = ['mil', 'mil', 'milhão', 'milhões', 'bilhão', 'bilhões', 'trilhão', '
 def abbreviator(element, limit=10):
   element = str(element)
   if element.isnumeric():
-    for i in range(4, len(element), 3):
-      if len(element) != 4:
-        if len(element) >= i and len(element) <= (i + 3):
+    for i in range(4, len(element) + 1, 3):
+      if i != 4:
+        if len(element) >= i and len(element) <= (i + 2):
           element = int(element)
           element /= (10 ** (i - 1))
           if element < 2:
             index = i - 5
           else:
             index = i - 4
+          if i == 4:
+            index += 1
           if not str(element).isdecimal():
             element = '{:.0f} {}'.format(element, tag[index])
           else:
