71847406e4fdc6f203a9e02b68e263209ce94e73
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2022-11-09 01:28:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ number = 1000
 limit = 500
 
 
+
+
 def abbreviator(element, limit):
-  index = 4
   if element.isdigit():
-    if len(element) >= index and len(element) <= (index + 2):
-      element /= (10 ** (index - 1))
-      element = "{:,2f} mil".format(element)
+    for i in range(4, len(element), 3):
+      if len(element) >= index and len(element) <= (index + 2):
+        element /= (10 ** (index - 1))
+        element = "{:,2f} mil".format(element)
   else:
     while element(limit) == '':
       limit -= 1
