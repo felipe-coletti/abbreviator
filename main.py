340bbd7d1205d8b0780768cc7c3d8cb798c2683e
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2022-11-09 00:17:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ limit = 500
 
 def abbreviator(element, limit):
   if element.isdigit():
-    if len(element) >= 1000 and len(element) < 1000000:
+    if len(element) >= 4 and len(element) < 7:
       element /= 1000
+      element = "{:,2f} mil".format(element)
   else:
     for i in range(limit, limit + 4):
-      element[i] = "."
+      element[i] = '.'
     for i in range(limit + 4, len(element)):
       element.remove[i]
 
